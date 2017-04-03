ff51f16fd53947702ad84c2d03bc514ddce8fb7f
Chris Roberson <chrisronline@gmail.com>
2017-04-02 22:02:16 -0400

    Tweaks

--- src/components/headshots/headshots.js
+++ src/components/headshots/headshots.js
@@ -9,8 +9,9 @@ class Headshots extends PureComponent {
     return (
       <section className="page page-headshots">
         <div className="headshots-list">
-          <img className="headshot" src="/assets/images/rsz_15.jpg" alt="Shauni Ruetz headshot" />
-          <img className="headshot" src="/assets/images/rsz_1rsz_4.jpg" alt="Shauni Ruetz headshot (2)" />
+          <img className="headshot" src="/assets/images/CHRIS1.png" alt="Shauni Ruetz headshot" />
+          <img className="headshot" src="/assets/images/CHRIS2.png" alt="Shauni Ruetz headshot (2)" />
+          <img className="headshot" src="/assets/images/CHRIS3.png" alt="Shauni Ruetz headshot (2)" />
         </div>
         <p>
           Picture credit: <a href="http://www.yaninamayphotography.com" target="_blank">www.yaninamayphotography.com</a>
