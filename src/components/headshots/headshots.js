c0a1190387d062bcfe0a16fc9539e9a78aa83551
Chris Roberson <chrisronline@gmail.com>
2017-04-04 20:18:58 -0400

    Tweaks

--- src/components/headshots/headshots.js
+++ src/components/headshots/headshots.js
@@ -1,17 +1,72 @@
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
+import Lightbox from 'react-images'
 
 if (process.env.BROWSER) {
   require('./headshots.scss')
 }
 
-class Headshots extends PureComponent {
+const images = [
+  { src: '/assets/images/CHRIS1.png' },
+  { src: '/assets/images/CHRIS2.png' },
+  { src: '/assets/images/CHRIS3.png' },
+]
+
+class Headshots extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentImage: 0,
+      isOpen: false,
+    }
+  }
+
+  prev() {
+    this.setState({
+      currentImage: Math.max(0, this.state.currentImage - 1),
+    })
+  }
+
+  next() {
+    this.setState({
+      currentImage: Math.min(images.length - 1, this.state.currentImage + 1),
+    })
+  }
+
+  closeLightbox() {
+    this.setState({
+      currentImage: 0,
+      isOpen: false,
+    })
+  }
+
+  openLightbox(index) {
+    this.setState({
+      currentImage: index,
+      isOpen: true,
+    })
+  }
+
   render() {
+    const localImages = images.map(({ src }, index) => {
+      return (
+        <img onClick={this.openLightbox.bind(this, index)}
+          className="headshot"
+          src={src}
+          alt="Shauni Ruetz headshot" />
+      )
+    })
     return (
       <section className="page page-headshots">
         <div className="headshots-list">
-          <img className="headshot" src="/assets/images/CHRIS1.png" alt="Shauni Ruetz headshot" />
-          <img className="headshot" src="/assets/images/CHRIS2.png" alt="Shauni Ruetz headshot (2)" />
-          <img className="headshot" src="/assets/images/CHRIS3.png" alt="Shauni Ruetz headshot (2)" />
+          {localImages}
+          <Lightbox
+            images={images}
+            currentImage={this.state.currentImage}
+            isOpen={this.state.isOpen}
+            onClickPrev={this.prev.bind(this)}
+            onClickNext={this.next.bind(this)}
+            onClose={this.closeLightbox.bind(this)}
+          />
         </div>
         <p>
           Picture credit: <a href="http://www.yaninamayphotography.com" target="_blank">www.yaninamayphotography.com</a>
